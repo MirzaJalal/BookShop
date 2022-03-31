e17988dadcd56a95bb71e3d2a05d1af6fcf131d7
Mirza Jalal Uddin <mirzajalal.uddin18@outlook.com>
2022-03-31 20:26:55 +0200

    Add project files.

---
+++ BookShopWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BookShopWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+
+            return View(objCategoryList);
+        }
+    }
+}
