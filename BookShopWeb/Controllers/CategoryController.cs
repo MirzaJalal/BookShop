b2c03cfab46b6a32bd0c8bf02960d97e61dfcf44
Mirza Jalal Uddin <mirzajalal.uddin18@outlook.com>
2022-04-08 23:00:04 +0200

    N Tier Projects

--- BookShopWeb/Controllers/CategoryController.cs
+++ BookShopWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BookShopWeb.Data;
+using BookShopWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookShopWeb.Controllers
@@ -17,6 +18,104 @@ namespace BookShopWeb.Controllers
             var objCategoryList = _db.Categories.ToList();
 
             return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            //if(category.name == category.displayorder.tostring()) //custom validation
+            //{
+            //    modelstate.addmodelerror("customerror", "the displayorder cannot exactly match the name");
+            //}
+
+            if (category.Name == category.DisplayOrder.ToString()) //Custom Validation
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryFromDb = _db.Categories.SingleOrDefault(c => c.Id == id);
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString()) //Custom Validation
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                TempData["success"] = "Category Updated Successfully!";
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult DeletePost(int id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+                _db.Categories.Remove(obj);
+            TempData["delete"] = "Category Deleted Successfully!";
+            _db.SaveChanges();
+                return RedirectToAction("Index");
         }
     }
 }
